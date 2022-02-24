3159fd4135dc87174370714fdd43956ac5f37d3c
XayrulloDev2005 <xayrullodev@gmail.com>
2022-02-24 13:47:38 +0500

    404 error

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import "./App.css"
+import Video from './video.mp4'
 
 function App() {
     return (
         <div className="App">
-            hello world
+            <video width="320" height="240" autoplay >
+                <source src={Video} type="video/mp4" />
+                <source src="movie.ogg" type="video/ogg"></source>
+                </video>
         </div>
     );
 }
